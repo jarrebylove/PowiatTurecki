282ee6a6103a0c32100e7d689e4b79e522935fc5
jarrebylove <jarrebylove@gmai.com>
2017-03-24 15:47:49 +0100

    JSONSource etc...

---
+++ app/src/main/java/HTTP2String.java
@@ -0,0 +1,137 @@
+import android.os.AsyncTask;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HTTP2String {
+
+    private URL url;
+
+    public HTTP2String(String url_) {
+        try {
+            url = new URL(url_);
+        } catch (Exception e) {}
+    }
+
+    public String get() {
+        String response = null;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            response = stream2string(in);
+        } catch (Exception e) {}
+        return response;
+    }
+
+    private String stream2string(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {}
+        finally {
+            try {
+                is.close();
+            } catch (IOException e) {}
+        }
+        return sb.toString();
+    }
+}
+
+private class JSONSource extends AsyncTask<Void, Void, Void> {
+
+    private URL url;
+    private HTTP2String http2string;
+    private String response;
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+     }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+        http2string = new HTTP2String(url);
+        String response = sh.makeServiceCall(url);
+
+        Log.e(TAG, "Response from url: " + jsonStr);
+
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+
+                // Getting JSON Array node
+                JSONArray contacts = jsonObj.getJSONArray("contacts");
+
+                // looping through All Contacts
+                for (int i = 0; i < contacts.length(); i++) {
+                    JSONObject c = contacts.getJSONObject(i);
+
+                    String id = c.getString("id");
+                    String name = c.getString("name");
+                    String email = c.getString("email");
+                    String address = c.getString("address");
+                    String gender = c.getString("gender");
+
+                    // Phone node is JSON Object
+                    JSONObject phone = c.getJSONObject("phone");
+                    String mobile = phone.getString("mobile");
+                    String home = phone.getString("home");
+                    String office = phone.getString("office");
+
+                    // tmp hash map for single contact
+                    HashMap<String, String> contact = new HashMap<>();
+
+                    // adding each child node to HashMap key => value
+                    contact.put("id", id);
+                    contact.put("name", name);
+                    contact.put("email", email);
+                    contact.put("mobile", mobile);
+
+                    // adding contact to contact list
+                    contactList.add(contact);
+                }
+            } catch (final JSONException e) {
+                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Json parsing error: " + e.getMessage(),
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+        } else {
+            Log.e(TAG, "Couldn't get json from server.");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),
+                            "Couldn't get json from server. Check LogCat for possible errors!",
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+            });
+
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+    }
+
+}
--- app/src/main/java/pl/turek/powiat/powiatturecki/MainActivity.java
+++ app/src/main/java/pl/turek/powiat/powiatturecki/MainActivity.java
@@ -6,7 +6,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView news_group_list_view ;
+    private ArrayAdapter<String> news_group_adapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+
+        news_group_list_view = (ListView) findViewById(R.id.NewsGroupListView);
+        String cars[] = {"Mercedes", "Fiat", "Ferrari", "Aston Martin", "Lamborghini", "Skoda", "Volkswagen", "Audi", "Citroen"};
+        ArrayList<String> carL = new ArrayList<String>();
+        carL.addAll( Arrays.asList(cars) );
+        news_group_adapter = new ArrayAdapter<String>(this, R.layout.news_group, R.id.textView3, carL);
+        news_group_list_view.setAdapter(news_group_adapter);
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
