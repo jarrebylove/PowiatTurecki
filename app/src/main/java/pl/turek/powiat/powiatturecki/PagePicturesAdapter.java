83e723ad7c1640ccf11869e28a7f331fa6720254
Jarre Bylove <jarrebylove@gmail.com>
2017-03-31 07:09:20 +0200

    scrolling...

--- app/src/main/java/pl/turek/powiat/powiatturecki/PagePicturesAdapter.java
+++ app/src/main/java/pl/turek/powiat/powiatturecki/PagePicturesAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
@@ -15,25 +16,28 @@ import com.squareup.picasso.Picasso;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-public class PagePicturesAdapter extends BaseAdapter {
+public class PagePicturesAdapter extends ArrayAdapter {
 
     private Context context;
     private ArrayList<PageSource.Picture> pictures;
+    private LayoutInflater inflater;
 
     public PagePicturesAdapter(Context context, ArrayList<PageSource.Picture> pictures) {
+        super(context, R.layout.page_picture, pictures);
         this.context = context;
         this.pictures = pictures;
+        inflater = LayoutInflater.from(context);
     }
 
-    @Override
-    public PageSource.Picture getItem(int position) {
-        return pictures.get(position);
-    }
+    //@Override
+    //public PageSource.Picture getItem(int position) {
+    //    return pictures.get(position);
+    //}
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
+    //@Override
+    //public long getItemId(int position) {
+    //    return position;
+    //}
 
     @Override
     /*public View getView(int position, View convertView, ViewGroup parent) {
@@ -58,13 +62,16 @@ public class PagePicturesAdapter extends BaseAdapter {
             //gridView = inflater.inflate(R.layout.page_picture, null);
             //ImageView picture = (ImageView) gridView.findViewById(R.id.page_picture);
             //Picasso.with(context).load(getItem(position).thumb).into(picture);
-            picture = new ImageView(context);
-        } else {
+            //picture = new ImageView(context);
+            convertView = inflater.inflate(R.layout.page_picture, parent, false);
+
+        }// else {
             //gridView = (View) convertView;
-            picture = (ImageView) convertView;
-        }
-        Picasso.with(context).load(getItem(position).thumb).into(picture);
-        return picture;
+        //    picture = (ImageView) convertView;
+        //}
+        picture = (ImageView) convertView.findViewById(R.id.page_picture);
+        Picasso.with(context).load(pictures.get(position).thumb).placeholder(R.drawable.ic_turek_county_arms).into(picture);
+        return convertView;
         //return gridView;
     }
 
