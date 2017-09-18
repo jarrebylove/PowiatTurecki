d7739b0bafe1d0bdebf3f0b5919f083d77c4dfaa
Jarre Bylove <jarrebylove@gmail.com>
2017-09-19 00:56:36 +0200

    add "listeners" instead extending classes

--- Powiat Turecki/src/main/java/pl/turek/powiat/powiatturecki/NewsGroupsPageAdapter.java
+++ Powiat Turecki/src/main/java/pl/turek/powiat/powiatturecki/NewsGroupsPageAdapter.java
@@ -3,6 +3,7 @@ package pl.turek.powiat.powiatturecki;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
