2ee4edfeda3dd70bc755eaa5ee64374593f52ae5
Jarre Bylove <jarrebylove@gmail.com>
2017-09-18 23:24:20 +0200

    add "listeners" instead extending classes

--- Powiat Turecki/src/main/java/pl/turek/powiat/powiatturecki/MessagesActivity.java
+++ Powiat Turecki/src/main/java/pl/turek/powiat/powiatturecki/MessagesActivity.java
@@ -1,35 +1,23 @@
 package pl.turek.powiat.powiatturecki;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MessagesActivity extends MainActivity {
+public class MessagesActivity extends MainActivity implements MessagesSourceListener {
 
-    class Messages extends MessagesSource {
-
-        private Activity activity;
-
-        public Messages(Activity activity) {
-            super();
-            this.activity = activity;
-        }
-
-        @Override
-        public void processResults(final ArrayList<Message> messages) {
-            if(messages != null) {
-                ListView messages_list_view = (ListView) findViewById(R.id.messages_listview);
-                MessagesListViewAdapter adapter = new MessagesListViewAdapter(MessagesActivity.this, messages);
-                messages_list_view.setAdapter(adapter);
-                loading_done();
-            } else {
-                loading_done();
-            }
+    @Override
+    public void onLoad(ArrayList<MessagesSource.Message> messages) {
+        if(messages != null) {
+            ListView messages_list_view = (ListView) findViewById(R.id.messages_listview);
+            MessagesListViewAdapter adapter = new MessagesListViewAdapter(MessagesActivity.this, messages);
+            messages_list_view.setAdapter(adapter);
+            loading_done();
+        } else {
+            loading_done();
         }
     }
 
@@ -39,7 +27,7 @@ public class MessagesActivity extends MainActivity {
         ViewStub stub = (ViewStub) findViewById(R.id.layout_content);
         stub.setLayoutResource(R.layout.messages);
         View inflated = stub.inflate();
-        Messages messages = new Messages(this);
-        messages.execute();
+        MessagesSource messages = new MessagesSource();
+        messages.addListener(this);
     }
 }
